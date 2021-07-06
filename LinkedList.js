1857ff6bc9ce15100596dcbe0cfb510926c99bb2
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-07-06 01:53:24 -0400

    updated helper functions

--- LinkedList.js
+++ LinkedList.js
@@ -1,12 +1,12 @@
 const Node = require('./Node');
 
-class LinkedList {
+class LinkedList { // First In First Out
   constructor() {
     this.head = null;
   }
 
-  addToHead(value) {
-    const nextNode = new Node(value);
+  addToHead(data) {
+    const nextNode = new Node(data);
     const currentHead = this.head;
     this.head = nextNode;
     if (currentHead) {
@@ -14,27 +14,60 @@ class LinkedList {
     }
   }
 
-  addToTail(value) {
+  addToTail(data) {
     let lastNode = this.head;
     if (!lastNode) {
-      this.head = new Node(value);
+      this.head = new Node(data);
     } else {
       let temp = this.head;
       while (temp.getNextNode() !== null) {
         temp = temp.getNextNode();
       }
-      temp.setNextNode(new Node(value));
+      temp.setNextNode(new Node(data));
     }
   }
 
   removeHead() {
     const removedHead = this.head;
-    if (!removedHead) return;
-
+    if (!removedHead) {
+      return;
+    }
     if (removedHead.next) {
       this.head = removedHead.next;
     }
     return removedHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.next;
+    }
+    output = output.concat("<tail>");
+    console.log(output);
+  }
+
+  findNodeIteratively(data) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  findNodeRecursively(data, currentNode = this.head) {
+    if (currentNode === null) {
+      return null;
+    } else if (currentNode.data === data) {
+      return currentNode;
+    } else {
+      return this.findNodeRecursively(data, currentNode.next);
+    }
   }
 
 }
