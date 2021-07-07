00bb4996b29a701d2f821f0be8e7059d56b79392
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-07-07 02:11:58 -0400

    added file containing sorting algorithms

---
+++ sorting.js
@@ -0,0 +1,84 @@
+const swap = (arr, indexOne, indexTwo) => {
+    const temp = arr[indexTwo];
+    arr[indexTwo] = arr[indexOne];
+    arr[indexOne] = temp;
+  };
+
+const bubbleSort = input => {
+  let swapCount = 0
+  let swapping = true;
+  
+  while (swapping) {
+    swapping = false;
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        swap(input, i, i + 1);
+        swapCount++;
+        swapping = true;
+      }
+    }
+  }
+  console.log(`Swapped ${swapCount} times`);
+  return input;
+};
+
+const mergeSort = (startArray) => {
+  const length = startArray.length;
+  if (length === 1) {
+    return startArray;
+  }
+  
+  const mid = Math.floor(length / 2);
+  const leftArray = startArray.slice(0, mid);
+  const rightArray = startArray.slice(mid, length);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray))
+}
+
+const merge = (leftArray, rightArray) => {
+  const sortedArray = [];
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    if (leftArray[0] < rightArray[0]) {
+      sortedArray.push(leftArray[0]);
+      leftArray.shift();
+    } else {
+      sortedArray.push(rightArray[0]);
+      rightArray.shift();
+    }
+  }
+  
+  return sortedArray.concat(leftArray).concat(rightArray);
+}
+
+const quicksort = (array, leftBound = 0, rightBound = array.length - 1) => {
+  if (leftBound < rightBound) {
+    const pivotIndex = partition(array, leftBound, rightBound);
+    quicksort(array, leftBound, pivotIndex - 1);
+    quicksort(array, pivotIndex, rightBound);
+  }
+  return array;
+}
+
+const partition = (array, leftIndex, rightIndex) => {
+  const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+  while (leftIndex <= rightIndex) {
+    while (array[leftIndex] < pivot) {
+      leftIndex++;
+    }
+    while (array[rightIndex] > pivot) {
+      rightIndex--;
+    }
+    if (leftIndex <= rightIndex) {
+      swap(array, leftIndex, rightIndex);
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+  return leftIndex;
+}
+
+module.exports = {
+  bubbleSort,
+  mergeSort,
+  quicksort
+}
